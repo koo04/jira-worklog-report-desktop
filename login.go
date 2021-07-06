d1a5e5f069a3cb725c790ec7a586126ec2234ab7
Douglas DeVicariis <kootg04@gmail.com>
2021-07-06 10:54:07 -0700

    config and dir permission update

--- login.go
+++ login.go
@@ -37,7 +37,7 @@ func (l *Login) WailsInit(runtime *wails.Runtime) error {
 	configFile = fmt.Sprintf("%s/config.json", configDir)
 
 	if _, err := os.Stat(configDir); os.IsNotExist(err) {
-		err := os.Mkdir(configDir, os.ModeDir)
+		err := os.Mkdir(configDir, 0666)
 		if err != nil {
 			return err
 		}
@@ -60,7 +60,7 @@ func (l *Login) WailsInit(runtime *wails.Runtime) error {
 }
 
 func (l *Login) Save(user string) error {
-	file, err := os.OpenFile(configFile, os.O_RDWR, 0600)
+	file, err := os.OpenFile(configFile, os.O_RDWR, 0666)
 	if err != nil {
 		l.Log.Errorf("Error getting config file: %v", err)
 		newFile, err := os.Create(configFile)
