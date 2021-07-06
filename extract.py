f2134d36fe9bc66714dd9ab2ba8daa6368d63e83
Douglas DeVicariis <kootg04@gmail.com>
2021-07-06 06:20:31 -0700

    Found a solution, maybe

--- extract.py
+++ extract.py
@@ -9,7 +9,7 @@ start = re.search(start_expression, i)
 end = re.search(end_expression, i)
 
 if start != None and end != None:
-    print(i[start.end()+1:end.start()-1].replace('\n', '\\\\\n'))
+    print(i[start.end()+1:end.start()-1])
 else:
     print("FAILED TO GET THE CHANGELOG")
     exit(1)
