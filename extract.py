3896d4366563b8e6fd61a98a0efc41767cb0925b
Douglas DeVicariis <kootg04@gmail.com>
2021-07-05 22:06:37 -0700

    Python script fix

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ import sys
 
 f = open("./CHANGELOG.md", "r")
 i = f.read()
-start_expression = r"## \[" + sys.argv[1] + r"\].+?(?=\n)"
+start_expression = r"## \[" + sys.argv[1].split('/')[2] + r"\].+?(?=\n)"
 end_expression = r"---"
 start = re.search(start_expression, i)
 end = re.search(end_expression, i)
