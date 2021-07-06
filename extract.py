b7fa47c0d7782010f910fbf68a65f948e8b47630
Douglas DeVicariis <kootg04@gmail.com>
2021-07-06 06:02:10 -0700

    Testing escaping new line in action output

--- extract.py
+++ extract.py
@@ -3,13 +3,16 @@ import sys
 
 f = open("./CHANGELOG.md", "r")
 i = f.read()
+print(sys.argv[1].split('/')[2])
 start_expression = r"## \[" + sys.argv[1].split('/')[2] + r"\].+?(?=\n)"
 end_expression = r"---"
 start = re.search(start_expression, i)
 end = re.search(end_expression, i)
 
+print(start,end)
+
 if start != None and end != None:
-    print(i[start.end()+1:end.start()-1])
+    print(i[start.end()+1:end.start()-1].replace('\n', '\\\\$\n'))
 else:
     print("FAILED TO GET THE CHANGELOG")
     exit(1)
